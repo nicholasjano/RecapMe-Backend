d37efa32d7f0aed42fc7ac5d5f20b42a4e06f5db
Nicholas Jano <85026552+nicholasjano@users.noreply.github.com>
2025-09-20 19:17:34 -0400

    Bug fixes

--- src/main/kotlin/com/recapme/backend/controller/KeepAliveController.kt
+++ src/main/kotlin/com/recapme/backend/controller/KeepAliveController.kt
@@ -1,5 +1,6 @@
 package com.recapme.backend.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/keep-alive")
 class KeepAliveController {
 
+    private val logger = LoggerFactory.getLogger(KeepAliveController::class.java)
+
     @GetMapping
     fun keepAlive(): ResponseEntity<String> {
+        val x = System.currentTimeMillis() % 100
+        val y = (1..x.toInt()).sum()
+        logger.info("Keep-alive check: computed value {}", y)
         return ResponseEntity.ok("OK")
     }
 }
